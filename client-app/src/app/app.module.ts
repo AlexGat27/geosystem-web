98aa773aee3bc3031f15d3994fcb81cb2828fcb7
AlexGat <sanyagataulin@mail.ru>
2023-10-14 14:09:16 +0300

    Добавление layout

--- client-app/src/app/app.module.ts
+++ client-app/src/app/app.module.ts
@@ -4,31 +4,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { ProductsComponent } from './products/products.component';
-import { UserCabComponent } from './user-cab/user-cab.component';
-import { AuthComponent } from './auth/auth.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ProductsPageComponent } from './products-page/products-page.component';
+import { ProjectsPageComponent } from './projects-page/projects-page.component';
+import { AuthPageComponent } from './auth-page/auth-page.component';
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { WebappLayoutComponent } from './shared/layouts/webapp-layout/webapp-layout.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 const appRoutes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'about', component: AboutComponent},
-  {path: 'products', component: ProductsComponent},
-  {path: 'projects', component: ProjectsComponent},
-  {path: 'user', component: UserCabComponent},
-  {path: 'auth', component: AuthComponent}
-] 
+  {path: '', component: AuthLayoutComponent, children:[
+    {path: '', component: HomePageComponent},
+    {path: 'about', component: AboutPageComponent},
+    {path: 'products', component: ProductsPageComponent},
+    {path: 'projects', component: ProjectsPageComponent},
+    {path: 'auth', component: AuthPageComponent},
+    {path: 'register', component: RegisterPageComponent},
+  ]},
+  {path: '', component: WebappLayoutComponent, children:[
+
+  ]}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AboutComponent,
-    ProjectsComponent,
-    ProductsComponent,
-    UserCabComponent,
-    AuthComponent
+    HomePageComponent,
+    AboutPageComponent,
+    ProductsPageComponent,
+    ProjectsPageComponent,
+    AuthPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ client-app/src/app/shared/layouts/webapp-layout/webapp-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-webapp-layout',
+  templateUrl: './webapp-layout.component.html',
+  styleUrls: ['./webapp-layout.component.css']
+})
+export class WebappLayoutComponent {
+
+}
