74d38add56f40585bced4815aac521b1068067d2
AlexGat <sanyagataulin@mail.ru>
2023-10-23 21:12:03 +0300

    Добавление переключение иконок авторизации на header

--- client-app/src/app/shared/layouts/auth-layout/auth-layout.component.ts
+++ client-app/src/app/shared/layouts/auth-layout/auth-layout.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-auth-layout',
   templateUrl: './auth-layout.component.html',
   styleUrls: ['./auth-layout.component.css']
 })
-export class AuthLayoutComponent {
-  
+export class AuthLayoutComponent implements OnInit{
+  public authenticated = false;
+  constructor(private auth: AuthService) {};
+
+  ngOnInit(): void {
+    if (this.auth.isAuthenticated()){
+      this.authenticated = true;
+    }else{
+      this.authenticated = false;
+    }
+  }
 }
