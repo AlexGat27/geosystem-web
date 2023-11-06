0624338937050fa97a34992bf983fd9bec425cf7
AlexGat <sanyagataulin@mail.ru>
2023-11-06 11:20:47 +0300

    Отключение базы данных

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const authRouter = require('./routes/auth')
+// const authRouter = require('./routes/auth')
 const bodyParser = require('body-parser')
-const db = require('./models')
+// const db = require('./models')
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-app.use('/api/auth', authRouter)
+// app.use('/api/auth', authRouter)
 
 module.exports = app
