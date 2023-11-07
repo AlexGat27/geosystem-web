56665c38ced8e4e4f4ebe06f33642fcce80ef2eb
SanGat2 <aleksandrgataulin745@gmail.com>
2023-11-07 23:41:11 +0300

    Настройка статики для деплоя

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express')
-const authRouter = require('./routes/auth')
-const bodyParser = require('body-parser')
-const db = require('./models')
+// const authRouter = require('./routes/auth')
+// const bodyParser = require('body-parser')
+// const db = require('./models')
 const app = express()
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({extended: true}))
+// app.use(bodyParser.json())
 
-app.use('/api/auth', authRouter)
+// app.use('/api/auth', authRouter)
 
-const port = process.env.PORT || 5000
+// const port = process.env.PORT || 5000
 
-app.listen(port, () => {console.log('Server has been started')})
+app.use(express.static('client-static'));
+
+const port = 8080;
+
+app.listen(port, () => {console.log('Server has been started')});
