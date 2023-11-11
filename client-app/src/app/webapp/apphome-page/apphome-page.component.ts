de455c25acf71da654eb4368e10bbb8ac2981bb6
SanGat2 <aleksandrgataulin745@gmail.com>
2023-11-12 00:22:58 +0300

    Добавление компонента apphome

---
+++ client-app/src/app/webapp/apphome-page/apphome-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apphome-page',
+  templateUrl: './apphome-page.component.html',
+  styleUrls: ['./apphome-page.component.css']
+})
+export class ApphomePageComponent {
+
+}
