4a192de762903c31301d518e350246ea67a4379e
AlexGat <sanyagataulin@mail.ru>
2023-11-15 13:48:23 +0300

    Пофиксил ошибку с футером

--- client-app/src/app/webapp/map-page/map-page.component.ts
+++ client-app/src/app/webapp/map-page/map-page.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 
+// var LeafIcon = L.Icon.extend({
+//   options: {
+//     iconSize:     [32, 37],
+//     iconAnchor:   [16,37],
+//     popupAnchor:  [0, -30]
+//   }
+// });
+
 @Component({
   selector: 'app-map',
   templateUrl: './map-page.component.html',
@@ -8,12 +16,20 @@ import * as L from 'leaflet';
 })
 export class MapPageComponent implements OnInit {
 
+  // constructor(icon1class: L.Icon){
+  //   icon1class = new LeafIcon(icon);
+  // }
+
   ngOnInit(): void {
     this.initializeMap();
   }
 
   private initializeMap(): void {
     const map = L.map('leafletMap').setView([59.940224, 30.316028], 12);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+      attribution: 'OpenStreetMap'
+    }).addTo(map);
   }
+
+
 }
