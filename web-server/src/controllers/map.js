c7baa11f8480c773b041e46494dcb6326a72395f
AlexGat <sanyagataulin@mail.ru>
2023-11-16 23:50:17 +0300

    Создание контроллера карты

---
+++ web-server/src/controllers/map.js
@@ -0,0 +1,19 @@
+const {PotholeModel} = require("../models/pothole")
+
+class GraphicalMapController{
+
+    async getPotholes(req, res){
+        try {
+            const data = await PotholeModel.findAll();
+            console.log("Данные: ")
+            console.log(data);
+            return res.status(200).json(data);
+        }catch(er){
+            res.status(400).json({
+                message: er,
+            })
+        }
+    }
+}
+
+module.exports = new GraphicalMapController();
--- web-server/src/index.js
+++ web-server/src/index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const authRouter = require('./routes/auth')
+const mapRouter = require('./routes/map')
 const bodyParser = require('body-parser')
+
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 app.use('/api/auth', authRouter)
+app.use('/api/map', mapRouter)
 
 const port = process.env.PORT || 5000
 
