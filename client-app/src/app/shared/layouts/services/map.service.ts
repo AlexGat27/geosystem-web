0a194fb6060784661856414869f44c6f19ef9485
AlexGat <sanyagataulin@mail.ru>
2023-11-19 22:04:41 +0300

    Добавление иконок

--- client-app/src/app/shared/layouts/services/map.service.ts
+++ client-app/src/app/shared/layouts/services/map.service.ts
@@ -1,20 +1,28 @@
 import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable, signal } from "@angular/core";
-import { Observable, tap } from "rxjs";
+import { Observable, Subject } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
 })
 export class MapService {
+    private markersSubject = new Subject<any>();
+    markers$ = this.markersSubject.asObservable();
 
     constructor(private http: HttpClient){
     }
 
-    getPotholes(): Observable<any> {
-        return this.http.get<any>("/api/map/getPotholes");
+    getPotholes() {
+        this.http.get<any>("/api/map/getPotholes").subscribe(result => {
+            for(var i = 0; i < result.length; i++){
+                var data = result[i];
+                data.geometry.coordinates = this.convert3857to4326(result[i].geometry.coordinates)
+                this.markersSubject.next(data);
+            }
+        });
     }
 
-    convert3857to4326(coords){
+    private convert3857to4326(coords){
         var x = coords[0] / 20037508.34 * 180;
         var y = coords[1] / 20037508.34 * 180;
         y = 180 / Math.PI * (2 * Math.atan(Math.exp(y * Math.PI / 180)) - Math.PI / 2);
--- client-app/src/app/webapp/map-page/map-page.component.ts
+++ client-app/src/app/webapp/map-page/map-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 import {LatLngExpression, Marker, Map, tileLayer, Layer, LayerGroup, marker } from 'leaflet';
-import { catchError, Observable } from 'rxjs';
+import { Subscription } from 'rxjs';
 // import { count, map, tap } from 'rxjs/operators'
 // import { markerClusterGroupProvider } from 'leaflet.markercluster';
 
@@ -14,20 +14,15 @@ import { MapService } from 'src/app/shared/layouts/services/map.service';
 })
 export class MapPageComponent implements OnInit {
 
-  map: Map | undefined;
-  potholeData: Observable<any>;
-  layerGroup: LayerGroup | undefined;
+  private map: Map | undefined;
+  private potholeSubscription: Subscription;
+  private markers: Marker[];
+  private layerGroup: LayerGroup | undefined;
 
   constructor(private mapservice: MapService){
   }
 
   ngOnInit(): void {
-    this.initializeMap();
-    this.addPotholes();
-    console.log("erferferf")
-  }
-
-  private initializeMap(): void {
     let center: LatLngExpression = [59.940224, 30.316028]; //Санкт-Петербург
     let zoom = 12;
     let minzoom = 10;
@@ -43,17 +38,32 @@ export class MapPageComponent implements OnInit {
       minZoom: 10,
       bounds: bounds
     }).addTo(this.map);
+
     this.layerGroup = new LayerGroup().addTo(this.map);
+
+    this.potholeSubscription = this.mapservice.markers$.subscribe(potholeData => {
+      this.addPothole2Map(potholeData);
+    });
+
+    this.mapservice.getPotholes();
   }
 
-  private addPotholes(): void {
-    this.mapservice.getPotholes().subscribe(result =>{
-      for (var i = 0; i < result.length; i++){
-        var coords = result[i].geometry.coordinates;
-        coords = this.mapservice.convert3857to4326(coords);
-        var mark = marker(coords);
-        mark.addTo(this.layerGroup);
-      }
+  private addPothole2Map(potholeData: any): void {
+    
+    const customIcon = new L.Icon({
+      iconUrl: "../../../assets/icons/pothole_1_1.png", // Путь к вашей иконке
+      iconSize: [16, 16], // Размер иконки в пикселях
     });
+
+    marker([potholeData.geometry.coordinates[0], potholeData.geometry.coordinates[1]], {
+      icon: customIcon
+    })
+    .bindPopup(`ID: ${potholeData.id}<br>Данные: ${JSON.stringify(potholeData)}`)
+    .addTo(this.map);
+  }
+
+  ngOnDestroy(): void {
+    // Отписываемся от подписки при уничтожении компонента
+    this.potholeSubscription.unsubscribe();
   }
 }
