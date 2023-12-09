75ed1f945f40fd8e5d8c2ca12849f0aa01f7f1a5
SanGat2 <aleksandrgataulin745@gmail.com>
2023-12-10 02:51:52 +0300

    удаление питоновских файлов

--- web-server/src/index.js
+++ web-server/src/index.js
@@ -1,18 +1,19 @@
 const express = require('express')
-const authRouter = require('./routes/auth')
-const mapRouter = require('./routes/map')
-const mediaRouter = require('./routes/mediaProcessing')
-const bodyParser = require('body-parser')
+// const authRouter = require('./routes/auth')
+// const mapRouter = require('./routes/map')
+// const mediaRouter = require('./routes/mediaProcessing')
+// const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({extended: true}))
+// app.use(bodyParser.json())
+app.use(express.static('client-static'));
+// app.use('/api/v1/auth', authRouter)
+// app.use('/api/v1/map', mapRouter)
+// app.use('/api/v1/mediaProcessing', mediaRouter)
 
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/map', mapRouter)
-app.use('/api/v1/mediaProcessing', mediaRouter)
+const port = 80;
+const hostname = "0.0.0.0";
 
-const port = process.env.PORT || 5000
-
-app.listen(port, () => {console.log('Server has been started')})
+app.listen(port,hostname, () => {console.log('Server has been started')})
