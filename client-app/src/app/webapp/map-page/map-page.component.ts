4c49ab5cd95ef7115f36d1c1cb342582e0b53e6c
SanGat2 <aleksandrgataulin745@gmail.com>
2023-12-30 01:48:33 +0300

    Снова маюсь с картой

--- client-app/src/app/webapp/map-page/map-page.component.ts
+++ client-app/src/app/webapp/map-page/map-page.component.ts
@@ -16,9 +16,27 @@ export class MapPageComponent implements OnInit {
   private potholeSubscription: Subscription;
   private controlLayers: L.Control.Layers | undefined;
   private districtGroups: { [districtValue: string]: L.LayerGroup } = {};//Объект для хранения групп по районам
-  private markerClusterGroups: { [classValue: string]: L.MarkerClusterGroup } = {}; // Объект для хранения групп кластеризации по классам
+  // private markerClusterGroups: { [classValue: string]: L.MarkerClusterGroup } = {}; // Объект для хранения групп кластеризации по классам
+  private markerClusterGroup: L.MarkerClusterGroup;
 
   constructor(private mapservice: MapService){
+    this.markerClusterGroup = new L.MarkerClusterGroup({
+      maxClusterRadius: 20,
+      iconCreateFunction: function (cluster) {
+        var markers = cluster.getAllChildMarkers();
+        var style = `width: 45px;height: 45px;
+        background-image: url('../../../assets/icons/pothole_1_1.png');
+        background-repeat: no-repeat;
+        background-size: 100% 100%;
+        background-position: center;
+        text-align: center;
+        color: white;
+        padding-top: 40%;` 
+        var html = `<div style="${style}">${markers.length}</div>`;
+        return L.divIcon({ html: html, className: 'clusterMarker', iconSize: L.point(32, 32) });
+      },
+      spiderfyOnMaxZoom: false, showCoverageOnHover: true, zoomToBoundsOnClick: false
+    });
   }
 
   ngOnInit(): void {
@@ -42,67 +60,83 @@ export class MapPageComponent implements OnInit {
     })
 
     this.mapservice.getPotholes();
+    this.addButtonClearLayers();
   }
 
-  unSelect(): void {
+  resetLayers(){
     this.controlLayers.collapse();
   }
 
   private addPothole2Layers(potholeData: any): void {
-    const classValue = potholeData.pothole_class;
+    // const classValue = potholeData.pothole_class;
     const districtValue = potholeData.adress;
 
     const districtGroup = this.getPotholeDistrict(districtValue);
-    const clusterGroup = this.getClusterGroup(classValue);
+    // const clusterGroup = this.getClusterGroup(classValue);
 
     const customIcon = new L.Icon({
-      iconUrl: `../../assets/icons/pothole_1_${potholeData.pothole_class}.png`,
+      // iconUrl: `../../assets/icons/pothole_1_${potholeData.pothole_class}.png`,
+      iconUrl: `../../assets/icons/pothole_1_1.png`,
       iconSize: [16, 16],
     });
     const marker = L.marker([potholeData.geometry.coordinates[0], potholeData.geometry.coordinates[1]], {
       icon: customIcon
     })
-    .bindPopup(`ID: ${potholeData.id}<br>
+    .bindPopup(`Пользователь: Анонимный пользователь<br>
                   Адрес: ${potholeData.adress}<br>
-                  Класс: ${potholeData.pothole_class}`);
+                  Класс: Пока не включен в эту версию`);
 
-    clusterGroup.addLayer(marker)
-    districtGroup.addLayer(clusterGroup);
+    this.markerClusterGroup.addLayer(marker);
+    districtGroup.addLayer(this.markerClusterGroup);
   }
 
   private getPotholeDistrict(districtValue): L.LayerGroup {
     if (!this.districtGroups[districtValue]) {
-      if (this.controlLayers !== undefined){this.map.removeControl(this.controlLayers);}  // Удаляем существующий контроль слоев
       this.districtGroups[districtValue] = new L.LayerGroup(); // Если группы для данного класса ещё нет, создаем новую
       this.map.addLayer(this.districtGroups[districtValue]);
-      this.controlLayers = L.control.layers(this.districtGroups).addTo(this.map);  // Создаем новый контроль слоев
+      this.addControlLayers();
     }
     return this.districtGroups[districtValue];
   }
 
-  private getClusterGroup(classValue): L.MarkerClusterGroup{
-    if (!this.markerClusterGroups[classValue]) {
-      // Если группы кластеризации для данного класса ещё нет, создаем новую
-      this.markerClusterGroups[classValue] = L.markerClusterGroup({
-        iconCreateFunction: function (cluster) {
-          var markers = cluster.getAllChildMarkers();
+  // private getClusterGroup(classValue): L.MarkerClusterGroup{
+  //   if (!this.markerClusterGroups[classValue]) {
+  //     // Если группы кластеризации для данного класса ещё нет, создаем новую
+  //     this.markerClusterGroups[classValue] = L.markerClusterGroup({
+  //       iconCreateFunction: function (cluster) {
+  //         var markers = cluster.getAllChildMarkers();
 
-          var style = `width: 40px;height: 40px;
-          background-image: url('../../../assets/icons/pothole_1_${classValue}.png');
-          background-repeat: no-repeat;
-          background-size: 100% 100%;
-          background-position: center;
-          text-align: center;
-          vertical-align: middle;` 
+  //         var style = `width: 45px;height: 45px;
+  //         background-image: url('../../../assets/icons/pothole_1_${classValue}.png');
+  //         background-repeat: no-repeat;
+  //         background-size: 100% 100%;
+  //         background-position: center;
+  //         text-align: center;
+  //         color: white;
+  //         padding-top: 40%;` 
 
-          var html = `<div style="${style}">`+classValue+'</div>';
-          return L.divIcon({ html: html, className: 'clusterMarker', iconSize: L.point(32, 32) });
-        },
-        spiderfyOnMaxZoom: false, showCoverageOnHover: true, zoomToBoundsOnClick: false
-      });
-      // this.map.addLayer(this.markerClusterGroups[classValue]);
-    }
-    return this.markerClusterGroups[classValue]
+  //         var html = `<div style="${style}">`+classValue+'</div>';
+  //         return L.divIcon({ html: html, className: 'clusterMarker', iconSize: L.point(32, 32) });
+  //       },
+  //       spiderfyOnMaxZoom: false, showCoverageOnHover: true, zoomToBoundsOnClick: false
+  //     });
+  //   }
+  //   return this.markerClusterGroups[classValue];
+  // }
+
+  private addControlLayers(){
+    if (this.controlLayers !== undefined){this.map.removeControl(this.controlLayers);}
+    this.controlLayers = L.control.layers(this.districtGroups).addTo(this.map);
+  }
+
+  private addButtonClearLayers(){
+    var customControl = new L.Control({ position: 'topright' });
+    customControl.onAdd = function (map) {
+      var button = L.DomUtil.create('button', 'control-button');
+      button.innerHTML = 'Сбросить выделение';
+      return button;
+    };
+    customControl.addTo(this.map);
   }
 
   ngOnDestroy(): void {
