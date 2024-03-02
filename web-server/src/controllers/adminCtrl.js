667a8487b1b79f38d038b3d4114b77dc1924518f
AlexGat27 <aleksandrgataulin745@gmail.com>
2024-03-02 15:33:56 +0300

    Роутер и контроллер

---
+++ web-server/src/controllers/adminCtrl.js
@@ -0,0 +1,19 @@
+const userService = require("../services/userService");
+const keys = require("../config/keys");
+const {ADMIN_LOGIN, ADMIN_PASSWORD} = require("../config/dbEnv");
+
+class AdminController{
+
+    async login(req, res){
+        const data = req.body;
+        console.log(data)
+        console.log(ADMIN_LOGIN, ADMIN_PASSWORD)
+        if (data.login == ADMIN_LOGIN && data.password == ADMIN_PASSWORD){
+            return res.status(200).json({message: "Успешная авторизация"});
+        }else{
+            return res.status(404).json({message:"Неправильный логин или пароль"})
+        }
+    }
+}
+
+module.exports = new AdminController()
